81fde71df48f9c4f6a2725c55b4c9edd89561ec0
geijinchan <abhishekravikumar24@gmail.com>
2026-09-10 03:02:59 +0530

    dockerized

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -17,7 +18,7 @@ const nextConfig: NextConfig = {
               key: 'XTransformPort',
             },
           ],
-          destination: 'http://127.0.0.1:3003/:path*',
+          destination: `${process.env.BACKEND_URL || 'http://127.0.0.1:3003'}/:path*`,
         },
       ],
     };
